2c62117009e8b26f692e14ed2e4bc4372bf135ed
Vishal Chaubey <vishalc9918@gmail.com>
2026-06-12 11:43:29 +0530

    first commit

---
+++ src/API/authAPI.js
@@ -0,0 +1,15 @@
+import API from "./axios";
+export const registerUser = (data) =>{
+    return API.post("/auth/register",data);
+}
+
+export const loginUser = (data) =>{
+    return API.post("/auth/login",data);
+}
+export const getProfile = () =>{
+    return API.get("/auth/profile");
+}
+
+export const updateProfile = (data) =>{
+    return API.put("/auth/profile",data);
+}
---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <div>
+            Loader Page
+        </div>
+    )
+}
+
+export default Loader
